07437eba38fed64c8daf354f692d827563581071
Neuxs0 <neuxsyt@gmail.com>
2025-04-25 12:06:22 -0700

    Remove modloader test logging

--- src/puzzle/main/java/dev/neuxs/mod_id/PuzzleMod.java
+++ src/puzzle/main/java/dev/neuxs/mod_id/PuzzleMod.java
@@ -2,10 +2,9 @@ package dev.neuxs.mod_id;
 
 import com.github.puzzle.core.loader.launch.provider.mod.entrypoint.impls.ClientModInitializer;
 
-@SuppressWarnings("unused")
 public class PuzzleMod implements ClientModInitializer {
     @Override
     public void onInit() {
-        Mod.init("Puzzle Loader");
+        Mod.init();
     }
 }
--- src/quilt/main/java/dev/neuxs/mod_id/QuiltMod.java
+++ src/quilt/main/java/dev/neuxs/mod_id/QuiltMod.java
@@ -3,10 +3,9 @@ package dev.neuxs.mod_id;
 import dev.crmodders.cosmicquilt.api.entrypoint.ModInitializer;
 import org.quiltmc.loader.api.ModContainer;
 
-@SuppressWarnings("unused")
 public class QuiltMod implements ModInitializer {
 	@Override
 	public void onInitialize(ModContainer mod) {
-		Mod.init("Cosmic Quilt");
+		Mod.init();
 	}
 }
